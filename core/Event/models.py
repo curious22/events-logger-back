7c609c2a3f6acc13f7c6034662b3e49937267c51
Denys Ponomarov <tenstepsint@gmail.com>
2023-09-29 13:48:18 +0300

    EventLog model

--- core/Event/models.py
+++ core/Event/models.py
@@ -6,3 +6,13 @@ class Event(models.Model):
 
     class Meta:
         db_table = "events"
+
+
+
+class EventLog(models.Model):
+    participan = models.ForeignKey("Participant", on_delete=models.CASCADE,)
+    event = models.ForeignKey(on_delete=models.PROTECT)
+    created_stamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "event_logs"
