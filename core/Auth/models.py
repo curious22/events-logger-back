7c3c4efea7d975e0b1be417fca377ba1dd6d7761
Denys Ponomarov <tenstepsint@gmail.com>
2023-10-24 15:14:10 +0300

    миграции

--- core/Auth/models.py
+++ core/Auth/models.py
@@ -7,20 +7,20 @@ class Token(models.Model):
     name = models.CharField(max_length=100)
     data = models.CharField(max_length=64, default=secrets.token_hex, unique=True)
     is_active = models.BooleanField(default=True)
-    expared_at = models.DateField()
+    expired_at = models.DateField(null=True, blank=True)
 
     class Meta:
         db_table = "tokens"
 
     @classmethod
-    def create_token(name, expared_at=None):
-        data = Token(name=name)
-        if expared_at:
-            data.expared_at = expared_at
+    def create_token(cls, name, expired_at=None):
+        data = cls(name=name)
+        if expired_at:
+            data.expired_at = expired_at
         while True:
             new_data = secrets.token_hex()
-            if not Token.objects.filter(token=new_data).exists():
-                data.token = new_data
+            if not cls.objects.filter(data=new_data).exists():
+                data.data = new_data
                 break
         data.save()
         return data
