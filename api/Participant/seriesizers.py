91c223526aa6293bbf730387ba835a5d770769ec
Denys Ponomarov <tenstepsint@gmail.com>
2023-11-27 16:30:30 +0200

    bad code(

--- api/Participant/seriesizers.py
+++ api/Participant/seriesizers.py
@@ -5,3 +5,8 @@ class ParticipantCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Participant
         fields = ('telegram_id',)
+
+    def validate_telegram_id(self, value):
+        if Participant.objects.filter(telegram_id=value).exists():
+            raise serializers.ValidationError("Участник с таким telegram_id уже существует.")
+        return value
